c826614d7885a42db9798b8673729158a184e422
Andrii Kucherenko <andrii_kucherenko@epam.com>
2014-09-22 08:09:14 +0300

    changed code

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -3,16 +3,16 @@
 
 class StringCalculatorTest extends PHPUnit_Framework_TestCase {
 
-    private $sut;
+    private $_sut;
 
     public function setUp() {
-        $this->sut = new \StringCalculator();
+        $this->_sut = new \StringCalculator();
     }
 
     /**
      * @test
      */
     public function shouldBeTrue() {
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }
